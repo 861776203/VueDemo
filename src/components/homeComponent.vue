cfa0d6603ef7d31fc0f3891f50e86ab713a456da
马宇狄 <mayud0_0@163.com>
2020-03-21 15:00:49 +0800

    up

---
+++ src/components/homeComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="show" class="sync_box">
+        <p>默认初始值是true,显示</p>
+        <button @click="close">关闭</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['show'],
+    data() {
+        return {}
+    },
+    methods: {
+        close() {
+            this.$emit('update:show', false)
+        }
+    }
+}
+</script>
+
+<style lang='scss'>
+.sync_box {
+    width: 200px;
+    border: 1px solid #eee;
+    margin: 0 auto;
+    padding: 20px 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="home">
+        <HomeComponent :show.sync="isclock"></HomeComponent>
+        <van-button type="info" size="mini" @click="open" style="marginTop:20px">打开</van-button>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeComponent from '@/components/homeComponent.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        HomeComponent
+    },
+    data() {
+        return {
+            isclock: true
+        }
+    },
+    methods: {
+        open() {
+            this.isclock = !this.isclock
+        }
+    }
+}
+</script>
